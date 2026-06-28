b9157ae94954b718e8fc397772618cd3249798d4
manish jakhar <manish946050@gmail.com>
2026-06-28 17:27:11 +0530

    first commit

---
+++ backend/lib/db.js
@@ -0,0 +1,13 @@
+import { setServers } from "dns";
+setServers(["8.8.8.8", "1.1.1.1"]); 
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.log("Error connecting to MONGODB", error.message);
+		process.exit(1);
+	}
+};
